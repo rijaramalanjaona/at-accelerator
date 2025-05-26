5b3793515890fbf81603c7fab71c0cfed9fa3468
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2025-05-26 11:24:33 +0200

    Exercise 6 : Custom Directive and Pipe

--- src/app/tv-show-table/tv-show-table.component.ts
+++ src/app/tv-show-table/tv-show-table.component.ts
@@ -1,19 +1,17 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { TvShow } from '../types';
-import { FavoritesService } from '../favorites.service';
 import { RouterLink } from '@angular/router';
+import { ToggleFavoriteDirective } from '../toggle-favorite.directive';
 
 @Component({
   selector: 'app-tv-show-table',
   standalone: true,
-  imports: [DatePipe, RouterLink],
+  imports: [DatePipe, RouterLink, ToggleFavoriteDirective],
   templateUrl: './tv-show-table.component.html',
   styleUrls: ['./tv-show-table.component.css']
 })
 export class TvShowTableComponent {
-  protected favoritesService = inject(FavoritesService);
-
   @Input({required: true})
   shows!: TvShow[];
 
